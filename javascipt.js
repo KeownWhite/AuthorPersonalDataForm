e0938bf2f65ec798f198b4c73f6a71510b9e0ac2
PrinceKrisyx <keownrwhite@gmail.com>
2016-07-01 15:43:25 +0000

    First commit

--- javascipt.js
+++ javascipt.js
@@ -1,43 +1,102 @@
-$('.form').find('input, textarea').on('keyup blur focus', function(e) 
+function validateForm()
 {
-
-  var $this = $(this),
-    label = $this.prev('label');
-
-  if (e.type === 'keyup') {
-    if ($this.val() === '') {
-      label.removeClass('active highlight');
-    } else {
-      label.addClass('active highlight');
-    }
-  } else if (e.type === 'blur') {
-    if ($this.val() === '') {
-      label.removeClass('active highlight');
-    } else {
-      label.removeClass('highlight');
-    }
-  } else if (e.type === 'focus') {
-
-    if ($this.val() === '') {
-      label.removeClass('highlight');
-    } else if ($this.val() !== '') {
-      label.addClass('highlight');
-    }
-  }
-
-});
-
-$('.tab a').on('click', function(e) {
-
-  e.preventDefault();
-
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-
-  target = $(this).attr('href');
-
-  $('.tab-content > div').not(target).hide();
-
-  $(target).fadeIn(600);
-
-});
+    var first_name = document.forms["contact_info"]["first_name"].value;
+    var middle_name = document.forms["contact_info"]["middle_name"].value;
+    var last_name = document.forms["contact_info"]["last_name"].value;
+    var date = document.forms["contact_info"]["date"].value;
+    var email = document.forms["contact_info"]["email"].value;
+    var nationality = document.forms["contact_info"]["nationality"].value;
+    var address = document.forms["contact_info"]["address"].value;
+    var apt = document.forms["contact_info"]["apt"].value;
+    var city = document.forms["contact_info"]["city"].value;
+    var country = document.forms["contact_info"]["country"].value;
+    var state = document.forms["contact_info"]["state"].value;
+    var zip_code = document.forms["contact_info"]["zip_code"].value;
+    
+    var countryCode_mobile = document.forms["contact_info"]["countryCode_mobile"].value;
+    var mobile = document.forms["contact_info"]["mobile"].value;
+    var countryCode_home = document.forms["contact_info"]["countryCode_home"].value;
+    var home_phone = document.forms["contact_info"]["home_phone"].value;
+    var email = document.forms["contact_info"]["email"].value;
+    
+    var fb = document.forms["contact_info"]["fb"].value;
+    var skype = document.forms["contact_info"]["skype"].value;
+    var whatsapp = document.forms["contact_info"]["whatsapp"].value;
+    var twitter = document.forms["contact_info"]["twitter"].value;
+    var instagram = document.forms["contact_info"]["instagram"].value;
+    var google_plus = document.forms["contact_info"]["google_plus"].value;
+    
+    var work_name = document.forms["contact_info"]["work_name"].value;
+    var work_addr = document.forms["contact_info"]["work_addr"].value;
+    var work_city = document.forms["contact_info"]["work_city"].value;
+    var work_county = document.forms["contact_info"]["work_county"].value;
+    var work_state = document.forms["contact_info"]["work_state"].value;
+    var work_zip_code = document.forms["contact_info"]["work_zip_code"].value;
+    var work_email = document.forms["contact_info"]["work_email"].value;
+    
+    var countryCode_work1 = document.forms["contact_info"]["countryCode_work1"].value;
+    var work_phone1 = document.forms["contact_info"]["work_phone1"].value;
+    var ext1 = document.forms["contact_info"]["ext1"].value;
+    var countryCode_work2 = document.forms["contact_info"]["countryCode_work2"].value;
+    var work_phone2 = document.forms["contact_info"]["work_phone2"].value;
+    var ext2 = document.forms["contact_info"]["ext2"].value;
+    
+    alert(first_name);
+    alert(middle_name);
+    alert(last_name);
+    alert(date);
+    alert(email);
+    alert(nationality);
+    alert(address);
+    alert(apt);
+    alert(city);
+    alert(country);
+    alert(state);
+    alert(zip_code);
+    alert(countryCode_mobile);
+    alert(mobile);
+    alert(countryCode_home);
+    alert(home_phone);
+    alert(email);
+    alert(fb);
+    alert(skype);
+    alert(whatsapp);
+    alert(first_name);
+    alert(middle_name);
+    alert(last_name);
+    alert(date);
+    alert(email);
+    alert(nationality);
+    alert(address);
+    alert(apt);
+    alert(city);
+    alert(country);
+    alert(state);
+    alert(zip_code);
+    alert(countryCode_mobile);
+    alert(mobile);
+    alert(countryCode_home);
+    alert(home_phone);
+    alert(email);
+    alert(fb);
+    alert(skype);
+    alert(whatsapp);
+    alert(twitter);
+    alert(instagram);
+    alert(google_plus);
+    alert(date);
+    alert(work_name);
+    alert(nationality);
+    alert(work_addr);
+    alert(work_city);
+    alert(work_county);
+    alert(work_state);
+    alert(work_zip_code);
+    alert(work_email);
+    alert(countryCode_work1);
+    alert(mobile);
+    alert(ext1);
+    alert(countryCode_work2);
+    alert(work_phone2);
+    alert(ext2);
+}
